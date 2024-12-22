0228fec6166236badb62b1b482ac728cf802aca1
Afaaf Hariri <haririafaaf@gmail.com>
2024-12-22 09:59:42 +0530

    Mongo DB Cluster

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL = "mongodb://localhost:27017/MERNProject";
+export const mongoDBURL =
+  "mongodb+srv://haririafaaf:HTRpwqmgpUYaRhPP@clusterpersonal.wfgef.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPersonal";
